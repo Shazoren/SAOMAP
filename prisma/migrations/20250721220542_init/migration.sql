3caf1261076f4248c8d6fede2886d140d799f249
Shazoren <kafefakekafefake@gmail.com>
2025-07-22 00:07:32 +0200

    update bdd

--- prisma/migrations/20250721220542_init/migration.sql
+++ prisma/migrations/20250721220542_init/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "Waypoint" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "x" INTEGER NOT NULL,
     "y" INTEGER NOT NULL,
     "gameCoords" TEXT NOT NULL,
-    "category" TEXT NOT NULL
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "Waypoint_pkey" PRIMARY KEY ("id")
 );
